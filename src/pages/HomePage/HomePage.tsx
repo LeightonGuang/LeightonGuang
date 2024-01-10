5ed2fd632945e673e3df9a39b0cc1106fdce61f4
Leighton Guang <leighton.guang@gmail.com>
2024-01-10 20:05:45 +0000

    feat: create home page layout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,1 +1,26 @@
-export const HomePage = () => <main></main>;
+import "./HomePage.scss";
+import memoji from "../../assets/Leighton-memoji.png";
+
+function HomePage() {
+  return (
+    <section className="homePage">
+      <div className="homePage__container">
+        <div className="hero">
+          <div className="hero__container">
+            <div className="hero__container-left">
+              <h3 className="hero__greeting">Hi I'm</h3>
+              <h1 className="hero__name">Leighton Guang</h1>
+              <h3 className="hero__greeting">a</h3>
+              <h2 className="hero__title">Full Stack Developer</h2>
+            </div>
+            <div className="hero__container-right">
+              <img src={memoji} alt="memoji" className="hero__memoji" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HomePage;
