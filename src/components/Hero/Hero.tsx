060e6843c7672e3d29b9a38177bc53cd2322c742
Leighton Guang <leighton.guang@gmail.com>
2024-01-11 22:21:47 +0000

    feat: move hero to its own component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import "./Hero.scss";
+import memoji from "../../assets/Leighton-memoji.png";
+
+function Header() {
+  return (
+    <div className="hero">
+      <div className="hero__container">
+        <div className="hero__container-left">
+          <h3 className="hero__greeting">Hi I'm</h3>
+          <h1 className="hero__name">Leighton Guang</h1>
+          <h3 className="hero__greeting">a</h3>
+          <h2 className="hero__title">Full Stack Developer</h2>
+        </div>
+        <div className="hero__container-right">
+          <img src={memoji} alt="memoji" className="hero__memoji" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,23 +1,10 @@
 import "./HomePage.scss";
-import memoji from "../../assets/Leighton-memoji.png";
-
+import Hero from "../../components/Hero/Hero";
 function HomePage() {
   return (
     <section className="homePage">
       <div className="homePage__container">
-        <div className="hero">
-          <div className="hero__container">
-            <div className="hero__container-left">
-              <h3 className="hero__greeting">Hi I'm</h3>
-              <h1 className="hero__name">Leighton Guang</h1>
-              <h3 className="hero__greeting">a</h3>
-              <h2 className="hero__title">Full Stack Developer</h2>
-            </div>
-            <div className="hero__container-right">
-              <img src={memoji} alt="memoji" className="hero__memoji" />
-            </div>
-          </div>
-        </div>
+        <Hero />
       </div>
     </section>
   );
