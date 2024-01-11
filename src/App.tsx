58029a5ebee779379ee20ae29a151c8241015aa7
Leighton Guang <leighton.guang@gmail.com>
2024-01-11 04:08:51 +0000

    fix: route to leightonguang

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/leightonguang" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </div>
