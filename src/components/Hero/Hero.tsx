e5fbb016921005acf8151b566ab7ec4ccb6f414a
Leighton Guang <leighton.guang@gmail.com>
2024-01-12 02:13:45 +0000

    feat: group all the text into one container

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,10 +6,12 @@ function Header() {
     <div className="hero">
       <div className="hero__container">
         <div className="hero__container-left">
-          <h3 className="hero__greeting">Hi I'm</h3>
-          <h1 className="hero__name">Leighton Guang</h1>
-          <h3 className="hero__greeting">a</h3>
-          <h2 className="hero__title">Full Stack Developer</h2>
+          <div className="hero__message">
+            <h3 className="hero__greeting">Hi I'm</h3>
+            <h1 className="hero__name">Leighton Guang</h1>
+            <h3 className="hero__greeting">a</h3>
+            <h2 className="hero__title">Full Stack Developer</h2>
+          </div>
         </div>
         <div className="hero__container-right">
           <img src={memoji} alt="memoji" className="hero__memoji" />
