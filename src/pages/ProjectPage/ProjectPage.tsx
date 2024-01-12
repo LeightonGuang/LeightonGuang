c21736f8bfcd34f0c808fffa07a8a75f645e9a52
Leighton Guang <leighton.guang@gmail.com>
2024-01-12 02:04:24 +0000

    feat: create project page component

---
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -0,0 +1,13 @@
+import "./ProjectPage.scss";
+
+function ProjectPage() {
+  return (
+    <div className="project">
+      <div className="project__container">
+        <h1>Projects</h1>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectPage;
