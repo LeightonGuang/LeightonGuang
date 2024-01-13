90d2635894d4d7aa72ca08304ff4744f2e79f2a0
Leighton Guang <leighton.guang@gmail.com>
2024-01-13 00:28:50 +0000

    feat: add technology section to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import "./HomePage.scss";
 import Hero from "../../components/Hero/Hero";
+import Technology from "../../components/Technology/Technology";
 function HomePage() {
   return (
     <section className="homePage">
       <div className="homePage__container">
         <Hero />
+        <Technology />
       </div>
     </section>
   );
