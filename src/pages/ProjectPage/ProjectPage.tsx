996e74f7dd9f83fa84d5b91e8500a50271785854
Leighton Guang <leighton.guang@gmail.com>
2024-01-14 23:51:01 +0000

    feat: add new project card component

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,11 +1,21 @@
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import "./ProjectPage.scss";
 
 function ProjectPage() {
   return (
     <div className="project">
       <div className="project__container">
-        <h1>Projects</h1>
+        <h1 className="project__title">Projects</h1>
       </div>
+      <ul className="project__list">
+        <ProjectCard
+          projectName="Shopdex"
+          projectImgUrlList={[""]}
+          projectGithubLink={
+            "https://github.com/LeightonGuang/capstone-project"
+          }
+        />
+      </ul>
     </div>
   );
 }
