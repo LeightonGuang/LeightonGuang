e9607cada1ceb45d78f45fc9968c0e13e6cb05b5
Leighton Guang <leighton.guang@gmail.com>
2024-01-14 21:33:52 +0000

    feat: add project page routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage.tsx";
+import ProjectPage from "./pages/ProjectPage/ProjectPage.tsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/leightonguang" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectPage />} />
         </Routes>
       </BrowserRouter>
     </div>
