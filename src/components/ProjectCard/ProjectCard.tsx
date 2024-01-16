c6781a93af8ab0f940567276a485202bcfbfd73f
Leighton Guang <leighton.guang@gmail.com>
2024-01-16 05:07:19 +0000

    feat: handle multiple images and text

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -3,26 +3,46 @@ import "./ProjectCard.scss";
 interface ProjectCardProps {
   projectName: string;
   projectImgUrlList: Array<string>;
+  projectTechnologyList: Array<string>;
   projectGithubLink: string;
 }
 
 function ProjectCard({
   projectName,
   projectImgUrlList,
+  projectTechnologyList,
   projectGithubLink,
 }: ProjectCardProps) {
   return (
     <li className="project-card">
       <div className="project-card__container">
-        <h2>{projectName}</h2>
-        <ul>
+        <h2 className="project-card__name">{projectName}</h2>
+        <ul className="project-card__img-list">
           {projectImgUrlList.map((projectImgUrl, index) => (
-            <li key={index}>
-              <img src={projectImgUrl} alt={projectName} />
+            <li key={index} className="project-card__img-container">
+              <img
+                src={projectImgUrl}
+                alt={projectName}
+                className="project-card__img"
+              />
             </li>
           ))}
         </ul>
-        <a href={projectGithubLink}>Github</a>
+        <ul className="project-card__technology-list">
+          {projectTechnologyList.map((projectTechnology, index) => (
+            <li key={index} className="project-card__technology">
+              {projectTechnology}
+            </li>
+          ))}
+        </ul>
+        <a href={projectGithubLink} className="project-card__link">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            alt="github logo"
+            className="project-card__github-logo"
+          />
+          Github
+        </a>
       </div>
     </li>
   );
