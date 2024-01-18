0cddbefb563a84a30f5efbc29d5b30535d415100
Leighton Guang <leighton.guang@gmail.com>
2024-01-18 02:09:25 +0000

    feat: move project page to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import "./HomePage.scss";
 import Hero from "../../components/Hero/Hero";
 import Technology from "../../components/Technology/Technology";
+import ProjectPage from "../ProjectPage/ProjectPage";
 function HomePage() {
   return (
     <section className="homePage">
       <div className="homePage__container">
         <Hero />
         <Technology />
+        <ProjectPage />
       </div>
     </section>
   );
