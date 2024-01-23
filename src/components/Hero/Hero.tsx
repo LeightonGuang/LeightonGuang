1947904a359d9f2fb0289b9cf9e52cc6a6c755dc
Leighton Guang <leighton.guang@gmail.com>
2024-01-23 16:47:18 +0000

    feat: redesign the hero section for mobile view

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,15 +5,15 @@ function Header() {
   return (
     <div className="hero">
       <div className="hero__container">
-        <div className="hero__container-left">
+        <div className="hero__container-top">
           <div className="hero__message">
-            <h3 className="hero__greeting">Hi I'm</h3>
-            <h1 className="hero__name">Leighton Guang</h1>
+            <span className="hero__greeting">Hi I'm</span>{" "}
+            <span className="hero__name">Leighton Guang</span>
             <h3 className="hero__greeting">a</h3>
             <h2 className="hero__title">Full Stack Developer</h2>
           </div>
         </div>
-        <div className="hero__container-right">
+        <div className="hero__container-bottom">
           <img src={memoji} alt="memoji" className="hero__memoji" />
         </div>
       </div>
