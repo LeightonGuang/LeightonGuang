b45fb136f1403e482fea101a6a9e67e56b1ec367
Leighton Guang <leighton.guang@gmail.com>
2024-01-29 15:59:17 +0000

    feat: add animation to hero message

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,27 @@
 import "./Hero.scss";
 import memoji from "../../assets/Leighton-memoji.png";
+import { motion } from "framer-motion";
 
 function Header() {
   return (
     <div className="hero">
       <div className="hero__container">
         <div className="hero__container-top">
-          <div className="hero__message">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 1,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+            className="hero__message"
+          >
             <span className="hero__greeting">Hi I'm</span>
             <h2 className="hero__name">Leighton Guang</h2>
             <span className="hero__greeting">a</span>
             <h2 className="hero__title">Full Stack Developer</h2>
-          </div>
+          </motion.div>
         </div>
         <div className="hero__container-bottom">
           <img src={memoji} alt="memoji" className="hero__memoji" />
