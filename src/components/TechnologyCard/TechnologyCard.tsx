fafd53b95c60fd863a2329b2191f7be19cd859b0
Leighton Guang <leighton.guang@gmail.com>
2024-01-29 11:58:02 +0000

    feat: different style for desktop view

--- src/components/TechnologyCard/TechnologyCard.tsx
+++ src/components/TechnologyCard/TechnologyCard.tsx
@@ -4,20 +4,20 @@ interface TechnologyCardProps {
   technologyName: string;
   technologyLogo: string;
 }
-
 function TechnologyCard({
   technologyName,
   technologyLogo,
 }: TechnologyCardProps) {
   return (
     <li className="technology-card">
-      <h1 className="technology-card__name">{technologyName}</h1>
-
-      <img
-        src={technologyLogo}
-        alt={technologyName}
-        className="technology-card__logo"
-      />
+      <div className="technology-card__container">
+        <img
+          src={technologyLogo}
+          alt={technologyName}
+          className="technology-card__logo"
+        />
+        <h1 className="technology-card__name">{technologyName}</h1>
+      </div>
     </li>
   );
 }
