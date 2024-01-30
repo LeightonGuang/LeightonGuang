dc678e615514bf18b720093fbd365777279f16be
Leighton Guang <leighton.guang@gmail.com>
2024-01-30 23:52:18 +0000

    feat: new about page

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,22 @@
+import "./AboutPage.scss";
+
+function AboutPage() {
+  return (
+    <section className="aboutPage">
+      <div className="aboutPage__container">
+        <h1 className="aboutPage__title">About Me</h1>
+        <p className="aboutPage__description">
+          I am passionate about Software Engineering because I consider myself a
+          maker/creator at heart. I love creating things and giving people a
+          pleasant experience using it. I got into Software Engineering because
+          I found out that being a software engineer also allows me to be
+          creative simply through a computer, without relying on physical tools
+          or materials. As a quick problem solver, I hope I can apply my skills
+          to drive companies to new heights.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default AboutPage;
