414803af5cc0be1ae5afa9fa0521a98dea21a4cc
Leighton Guang <leighton.guang@gmail.com>
2024-01-31 18:34:43 +0000

    feat: add div for card styling

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -5,15 +5,17 @@ function AboutPage() {
     <section className="aboutPage">
       <div className="aboutPage__container">
         <h1 className="aboutPage__title">About Me</h1>
-        <p className="aboutPage__description">
-          I am passionate about Software Engineering because I consider myself a
-          maker/creator at heart. I love creating things and giving people a
-          pleasant experience using it. I got into Software Engineering because
-          I found out that being a software engineer also allows me to be
-          creative simply through a computer, without relying on physical tools
-          or materials. As a quick problem solver, I hope I can apply my skills
-          to drive companies to new heights.
-        </p>
+        <div className="aboutPage__card">
+          <p className="aboutPage__description">
+            Hi I'm Leighton. I am passionate about Software Engineering because
+            I consider myself a maker/creator at heart. I love creating things
+            and giving people a pleasant experience using it. I got into
+            Software Engineering because I found out that being a software
+            engineer allows me to be creative simply through a computer, without
+            relying on physical tools or materials. As a quick problem solver, I
+            hope I can apply my skills to drive companies to new heights.
+          </p>
+        </div>
       </div>
     </section>
   );
