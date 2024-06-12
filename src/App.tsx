f9e4f204297e9de8e1ebc4a5836957d58e272aab
Leighton Guang <leighton.guang@icloud.com>
2024-06-12 14:13:50 +0100

    feat: add route for animations page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import HomePage from "./pages/HomePage/HomePage.tsx";
 import ProjectPage from "./pages/ProjectPage/ProjectPage.tsx";
-import Footer from "./components/Footer/Footer.tsx";
-import AboutPage from "./pages/AboutPage/AboutPage.tsx";
 import PlaygroundPage from "./pages/PlaygroundPage/PlaygroundPage.tsx";
+import AnimationPage from "./pages/PlaygroundProjects/AnimationsPage/AnimationPage.tsx";
+import AboutPage from "./pages/AboutPage/AboutPage.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/LeightonGuang" element={<HomePage />} />
           <Route path="/projects" element={<ProjectPage />} />
           <Route path="/playground" element={<PlaygroundPage />} />
+          <Route path="/playground/animations" element={<AnimationPage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
         <Footer />
