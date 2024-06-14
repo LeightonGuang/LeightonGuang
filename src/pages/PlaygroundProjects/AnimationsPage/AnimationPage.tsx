837bd90f50613c40ec884605be297f168595879c
Leighton Guang <leighton.guang@icloud.com>
2024-06-14 13:54:32 +0100

    feat: add framer motion animation elements

--- src/pages/PlaygroundProjects/AnimationsPage/AnimationPage.tsx
+++ src/pages/PlaygroundProjects/AnimationsPage/AnimationPage.tsx
@@ -1,7 +1,65 @@
 import "./Animationpage.scss";
+import { motion } from "framer-motion";
 
 const animations = () => {
-  return <div>animations</div>;
+  return (
+    <section className="animations">
+      <div className="animations__container">
+        <h1 className="animations__title">Framer Motion Animations</h1>
+        <ul className="animations__list">
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            Hover and Click
+          </motion.li>
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            two
+          </motion.li>
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            three
+          </motion.li>
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            four
+          </motion.li>
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            four
+          </motion.li>
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            four
+          </motion.li>
+          <motion.li
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="animations__item"
+          >
+            four
+          </motion.li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default animations;
