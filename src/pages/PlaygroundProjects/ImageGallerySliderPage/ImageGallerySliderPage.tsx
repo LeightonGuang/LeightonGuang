f8e6e894b08b49a5b720be74983f0a775bb59bb6
Leighton Guang <leighton.guang@icloud.com>
2024-06-22 12:48:50 +0100

    feat: create Image Gallery Slider page

---
+++ src/pages/PlaygroundProjects/ImageGallerySliderPage/ImageGallerySliderPage.tsx
@@ -0,0 +1,12 @@
+import "./ImageGallerySliderPage.scss";
+
+const ImageGallerySliderPage = () => {
+  return (
+    <section className="imageGallerySlider">
+      <h1 className="imageGallerySlider__title">Image Gallery Slider</h1>
+      <div className="imageGallerySlider__container"></div>
+    </section>
+  );
+};
+
+export default ImageGallerySliderPage;
