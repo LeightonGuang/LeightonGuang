437fb8351a904446cd1b358e18aee50a9f7ea01c
Leighton Guang <leighton.guang@icloud.com>
2024-06-26 11:32:10 +0100

    feat: add left and right button

--- src/components/ImageGallerySlider/ImageGallerySlider.tsx
+++ src/components/ImageGallerySlider/ImageGallerySlider.tsx
@@ -4,14 +4,49 @@ import { useState } from "react";
 const ImageGallerySlider = ({ images }: { images: string[] }) => {
   const [imageIndex, setImageIndex] = useState(0);
 
+  const handleRightClick = () => {
+    if (imageIndex !== 0) {
+      setImageIndex(imageIndex - 1);
+    }
+  };
+
+  const handleLeftClick = () => {
+    if (imageIndex !== images.length - 1) {
+      setImageIndex(imageIndex + 1);
+    }
+  };
+
   return (
     <div className="imageGallerySlider">
       <div className="imageGallerySlider__container">
-        <img className="imageGallerySlider__img" src="" alt="" />
-        <ul>
+        <div className="imageGallerySlider__display">
+          <button
+            className="imageGallerySlider__display--btn"
+            onClick={handleRightClick}
+          >{`<`}</button>
+          <img
+            className="imageGallerySlider__display--img"
+            src={images[imageIndex]}
+            alt=""
+          />
+          <button
+            className="imageGallerySlider__display--btn"
+            onClick={handleLeftClick}
+          >{`>`}</button>
+        </div>
+        <ul className="imageGallerySlider__list">
           {images.map((image, index) => (
-            <li>
-              <img src={image} alt={`image ${index}`} />
+            <li
+              className={`imageGallerySlider__item ${
+                index === imageIndex ? "imageGallerySlider__item--active" : ""
+              }`}
+              key={index}
+            >
+              <img
+                className="imageGallerySlider__img"
+                src={image}
+                alt={`image ${index}`}
+              />
             </li>
           ))}
         </ul>
