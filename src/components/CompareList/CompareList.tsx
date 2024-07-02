61c2cb5180e55044bc66ec4b2e5dbed3cf9593ba
Leighton Guang <leighton.guang@icloud.com>
2024-07-02 16:34:32 +0100

    feat: add pinned list using state

--- src/components/CompareList/CompareList.tsx
+++ src/components/CompareList/CompareList.tsx
@@ -1,11 +1,40 @@
 import "./CompareList.scss";
 import { ItemObj } from "../../types/ItemObj";
+import { useState } from "react";
 
 const CompareComponent = ({ itemObjList }: { itemObjList: ItemObj[] }) => {
+  const [pinnedItemList, setPinnedItemList] = useState<ItemObj[]>([]);
+  const [unpinnedItemList, setUnpinnedItemList] =
+    useState<ItemObj[]>(itemObjList);
+
   return (
     <div className="compareList">
       <ul className="compareList__container">
-        {itemObjList.map((itemObj, index) => (
+        {
+          // don't show pinned list when it's empty
+          pinnedItemList.length !== 0
+            ? pinnedItemList.map((pinnedItemObj, index) => (
+                <li className="compareList__item" key={index}>
+                  <ul className="compareList__item-list">
+                    <img
+                      className="compareList__img"
+                      src={pinnedItemObj.imageUrl}
+                    />
+                    <li className="compareList__brand">
+                      {pinnedItemObj.brand}
+                    </li>
+                    <li className="compareList__name">{pinnedItemObj.name}</li>
+                    <li className="compareList__year">{pinnedItemObj.year}</li>
+                    <li className="compareList__price">
+                      ${pinnedItemObj.price}
+                    </li>
+                    <li className="compareList__description">desciption</li>
+                  </ul>
+                </li>
+              ))
+            : ``
+        }
+        {unpinnedItemList.map((itemObj, index) => (
           <li className="compareList__item" key={index}>
             <ul className="compareList__item-list">
               <img className="compareList__img" src={itemObj.imageUrl} />
