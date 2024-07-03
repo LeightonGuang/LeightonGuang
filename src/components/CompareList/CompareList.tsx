ece6448885900e924aa14d7a930d43b3010682fc
Leighton Guang <leighton.guang@icloud.com>
2024-07-03 21:24:05 +0100

    feat: working pin and unpin button for compare list

--- src/components/CompareList/CompareList.tsx
+++ src/components/CompareList/CompareList.tsx
@@ -3,9 +3,45 @@ import { ItemObj } from "../../types/ItemObj";
 import { useState } from "react";
 
 const CompareComponent = ({ itemObjList }: { itemObjList: ItemObj[] }) => {
+  // original order of the list
+  const orderedList: ItemObj[] = itemObjList;
   const [pinnedItemList, setPinnedItemList] = useState<ItemObj[]>([]);
   const [unpinnedItemList, setUnpinnedItemList] =
     useState<ItemObj[]>(itemObjList);
+
+  const handlePinButton = (objIndex: number) => {
+    setPinnedItemList([...pinnedItemList, unpinnedItemList[objIndex]]);
+    setUnpinnedItemList((prevList) =>
+      prevList.filter((_item, i) => i !== objIndex)
+    );
+  };
+
+  const handleUnpinButton = (objIndex: number) => {
+    if (!orderedList.includes(pinnedItemList[objIndex])) {
+      console.log(
+        `${pinnedItemList[objIndex].name} is not in the original list`
+      );
+      return;
+    }
+
+    if (unpinnedItemList.includes(pinnedItemList[objIndex])) {
+      console.log(
+        `${pinnedItemList[objIndex].name} is already in the unpinned list`
+      );
+      return;
+    }
+
+    for (let i: number = 0; i < orderedList.length; i++) {
+      const originalEl: ItemObj = orderedList[i];
+
+      if (pinnedItemList[objIndex] === originalEl) {
+        unpinnedItemList.splice(i, 0, pinnedItemList[objIndex]);
+        break;
+      }
+    }
+
+    setPinnedItemList(pinnedItemList.filter((_item, i) => i !== objIndex));
+  };
 
   return (
     <div className="compareList">
@@ -15,6 +51,14 @@ const CompareComponent = ({ itemObjList }: { itemObjList: ItemObj[] }) => {
           pinnedItemList.length !== 0
             ? pinnedItemList.map((pinnedItemObj, index) => (
                 <li className="compareList__item" key={index}>
+                  <button
+                    className="compareList__pin-button"
+                    onClick={() => {
+                      handleUnpinButton(index);
+                    }}
+                  >
+                    unpin
+                  </button>
                   <ul className="compareList__item-list">
                     <img
                       className="compareList__img"
@@ -36,6 +80,14 @@ const CompareComponent = ({ itemObjList }: { itemObjList: ItemObj[] }) => {
         }
         {unpinnedItemList.map((itemObj, index) => (
           <li className="compareList__item" key={index}>
+            <button
+              className="compareList__pin-button"
+              onClick={() => {
+                handlePinButton(index);
+              }}
+            >
+              pin
+            </button>
             <ul className="compareList__item-list">
               <img className="compareList__img" src={itemObj.imageUrl} />
               <li className="compareList__brand">{itemObj.brand}</li>
