1cc7721e957b9a01bb86f2cb81377b06ee643b1c
Leighton Guang <leighton.guang@icloud.com>
2024-07-07 00:43:02 +0100

    feat: add labels, inputs and submit button to form

--- src/components/AddCompareItemModal/AddCompareItemModal.tsx
+++ src/components/AddCompareItemModal/AddCompareItemModal.tsx
@@ -6,30 +6,66 @@ const AddCompareItemModal = ({
   setIsAddItemModal: (value: boolean) => void;
 }) => {
   return (
-    <>
-      <div className="addCompareItemModal">
-        <div className="addCompareItemModal__container">
-          <div className="addCompareItemModal__nav">
-            <h1 className="addCompareItemModal__title">Add Item</h1>
-            <button
-              className="addCompareItemModal__btn-close"
-              onClick={() => setIsAddItemModal(false)}
-            >
-              Close
-            </button>
-          </div>
-
-          <div className="addCompareItemModal__body">
-            <form action="">
-              <label htmlFor="">
-                Brand:
-                <input type="text" name="brand" />
-              </label>
-            </form>
-          </div>
+    <div className="addCompareItemModal">
+      <div className="addCompareItemModal__card">
+        <div className="addCompareItemModal__nav">
+          <h1 className="addCompareItemModal__title">Add Item</h1>
+          <button
+            className="addCompareItemModal__btn-close"
+            onClick={() => setIsAddItemModal(false)}
+          >
+            Close
+          </button>
         </div>
+
+        <form className="addCompareItemModal__form" action="">
+          <label htmlFor="">
+            Brand:
+            <input
+              className="addCompareItemModal__input"
+              type="text"
+              name="brand"
+            />
+          </label>
+          <label htmlFor="">
+            Name:
+            <input
+              className="addCompareItemModal__input"
+              type="text"
+              name="name"
+            />
+          </label>
+          <label htmlFor="">
+            Year:
+            <input
+              className="addCompareItemModal__input"
+              type="text"
+              name="year"
+            />
+          </label>
+          <label htmlFor="">
+            Price:
+            <input
+              className="addCompareItemModal__input"
+              type="text"
+              name="price"
+            />
+          </label>
+          <label htmlFor="">
+            Description:
+            <textarea
+              className="addCompareItemModal__textarea"
+              name="description"
+              id=""
+            />
+          </label>
+
+          <button className="addCompareItemModal__add-btn" type="submit">
+            Add
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
