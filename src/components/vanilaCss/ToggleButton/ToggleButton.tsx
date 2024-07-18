8e3a6c8365e3aac189659a0ac59cec8e6a43c06a
Leighton Guang <leighton.guang@icloud.com>
2024-07-18 06:51:32 +0100

    feat: working toggle button animation

--- src/components/vanilaCss/ToggleButton/ToggleButton.tsx
+++ src/components/vanilaCss/ToggleButton/ToggleButton.tsx
@@ -4,9 +4,6 @@ const ToggleButton = () => {
   return (
     <div className="toggleButton">
       <input className="toggleButton__checkbox" type="checkbox" id="checkbox" />
-      <label className="toggleButton__knob" htmlFor="checkbox">
-        checked
-      </label>
     </div>
   );
 };
