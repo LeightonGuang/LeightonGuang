4c4a2a6c22e46ce24a53931aaee36731410b4ec0
Leighton Guang <leighton.guang@gmail.com>
2025-01-15 03:49:12 +0000

    Feat: Updated social page

--- src/pages/SocialsPage/SocialsPage.tsx
+++ src/pages/SocialsPage/SocialsPage.tsx
@@ -1,69 +1,85 @@
 import "./SocialsPage.scss";
 import memoji from "../../assets/Leighton-memoji.png";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 
 const SocialsPage = () => {
-  const socialsList: { name: string; link: string; iconUrl: string }[] = [
-    {
-      name: "LinkedIn",
-      link: "https://www.linkedin.com/in/leighton-guang/",
-      iconUrl: "https://cdn-icons-png.flaticon.com/512/174/174857.png",
-    },
-    {
-      name: "GitHub",
-      link: "https://github.com/LeightonGuang",
-      iconUrl: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
-    },
-  ];
+  const BusinessCard = () => {
+    const technologies = [
+      "React",
+      "Typescript",
+      "Next.js",
+      "MySQL",
+      "Express.js",
+      "Supabase",
+    ];
+
+    const socialsList: { name: string; link: string; iconUrl: string }[] = [
+      {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/leighton-guang/",
+        iconUrl: "https://cdn-icons-png.flaticon.com/512/174/174857.png",
+      },
+      {
+        name: "GitHub",
+        link: "https://github.com/LeightonGuang",
+        iconUrl: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+      },
+    ];
+
+    return (
+      <div className="BusinessCard">
+        <div className="BusinessCard__container">
+          <div>
+            <h1 className="BusinessCard__name">Leighton Guang</h1>
+            <h2 className="BusinessCard__title">React Full Stack Developer</h2>
+            <span className="BusinessCard__location">London, UK</span>
+
+            <div className="BusinessCard__technologies">
+              {technologies.map((technology, i) => (
+                <span key={i} className="BusinessCard__technology">
+                  {technology}
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <img className="BusinessCard__logo" src={memoji} alt="logo" />
+        </div>
+
+        <div className="BusinessCard__footer">
+          <div className="BusinessCard__socials">
+            {socialsList.map((social, i) => (
+              <a
+                key={i}
+                href={social.link}
+                target="_blank"
+                className="BusinessCard__social"
+              >
+                <img
+                  className="BusinessCard__social-icon"
+                  src={social.iconUrl}
+                  alt={social.name}
+                />
+              </a>
+            ))}
+          </div>
+
+          <Link to={"/home"} className="BusinessCard__btn">
+            <div className="BusinessCard__btn-text">
+              <span>Continue</span>
+              <span>{"->"}</span>
+            </div>
+          </Link>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <section className="SocialsPage">
-      <motion.div
-        whileHover={{ scale: [null, 1.05] }}
-        transition={{ duration: 0.2 }}
-        className="SocialsPage__card"
-      >
-        <div>
-          <img className="SocialsPage__profile-img" alt="memoji" src={memoji} />
-          <h1 className="SocialsPage__name">Leighton Guang</h1>
-          <p className="SocialsPage__title">Full Stack React Developer</p>
-        </div>
-
-        <ul className="SocialsPage__links">
-          <li className="SocialsPage__link-item">
-            <Link
-              className="SocialsPage__link"
-              to="https://docs.google.com/document/d/1EX49XzP9tnGXlJuNHtFzzqrBXhm_GnSXQQymitoxS8A/edit?usp=sharing"
-            >
-              Resume
-            </Link>
-          </li>
-        </ul>
-
-        <ul className="SocialsPage__contact">
-          <li>leighton.guang@icloud.com</li>
-          <li>London, UK</li>
-        </ul>
-
-        <ul className="SocialsPage__socials">
-          {socialsList.map((socialObj) => (
-            <li key={socialObj.name} className="SocialsPage__social-item">
-              <a className="SocialsPage__social-link" href={socialObj.link}>
-                <img
-                  className="SocialsPage__social-icon"
-                  src={socialObj.iconUrl}
-                  alt={socialObj.name}
-                />
-              </a>
-            </li>
-          ))}
-        </ul>
-
-        <Link className="SocialsPage__redirect" to={"/home"}>
-          {"Continue to Website >>>"}
-        </Link>
-      </motion.div>
+      <div className="SocialsPage__container">
+        <BusinessCard />
+      </div>
     </section>
   );
 };
