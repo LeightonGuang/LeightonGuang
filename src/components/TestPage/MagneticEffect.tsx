15456f55f85362431d82bed12078806bcee24c98
LeightonGuang <leighton.guang@icloud.com>
2025-09-21 06:12:06 +0100

    Feat: Test comopnent for magnetic effect

---
+++ src/components/TestPage/MagneticEffect.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Magnetic from "../Magnetic";
+
+const MagneticEffect = () => {
+  return (
+    <div>
+      <Magnetic>
+        <span className="text-3xl font-bold">R</span>
+      </Magnetic>
+    </div>
+  );
+};
+
+export default MagneticEffect;
