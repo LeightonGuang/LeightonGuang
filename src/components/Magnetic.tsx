ca180876c9b6c9f47b94b6135a99631e6350baa9
LeightonGuang <leighton.guang@icloud.com>
2025-09-21 06:11:44 +0100

    Feat: New Framer Motion magnetic effect component

---
+++ src/components/Magnetic.tsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+
+const Magnetic = ({ children }: { children: React.ReactNode }) => {
+  const magnetRef = useRef<HTMLDivElement>(null);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const { clientX, clientY } = e;
+    if (!magnetRef.current) return;
+
+    const { width, height, left, top } =
+      magnetRef.current.getBoundingClientRect();
+
+    const x = clientX - (left + width / 2);
+    const y = clientY - (top + height / 2);
+
+    setPos({ x, y });
+  };
+
+  const handleMouseLeave = () => {
+    setPos({ x: 0, y: 0 });
+  };
+
+  return (
+    <motion.div
+      className="cursor-pointer"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      ref={magnetRef}
+      animate={{
+        x: pos.x,
+        y: pos.y,
+      }}
+      transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default Magnetic;
