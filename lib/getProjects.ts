fd9bb3a0b716662ece0577907c93cdba6a3421a3
LeightonGuang <leighton.guang@icloud.com>
2026-08-10 04:59:21 +0100

    feat: change date stored for date sorting

--- lib/getProjects.ts
+++ lib/getProjects.ts
@@ -1,7 +1,7 @@
 export interface Project {
 	title: string
 	description?: string
-	date: string
+	date: string // YYYY-MM
 	url?: string
 	technologies: string[]
 	github?: string
@@ -13,7 +13,7 @@ export function getProjects(): Project[] {
 		{
 			title: 'Saúde Brasil website',
 			description: 'An e-commerce website for Saúde Brasil that sells peptides in Brazil.',
-			date: 'Aug 2026',
+			date: '2026-08',
 			url: 'https://peptidesaude.com',
 			technologies: [
 				'Nextjs',
@@ -28,7 +28,7 @@ export function getProjects(): Project[] {
 		{
 			title: 'MV Steel Group website',
 			description: 'Description 1',
-			date: 'Oct 2025',
+			date: '2025-10',
 			url: 'https://mvsteelgroup.lt',
 			technologies: ['React', 'Next.js'],
 			images: ['https://example.com/image1.jpg', 'https://example.com/image2.jpg']
@@ -36,7 +36,7 @@ export function getProjects(): Project[] {
 		{
 			title: 'FMCG Innovations website',
 			description: '',
-			date: 'Apr 2026',
+			date: '2026-04',
 			url: 'https://fmcginnovations.com',
 			technologies: [],
 			images: []
@@ -45,7 +45,7 @@ export function getProjects(): Project[] {
 			title: 'Twitch Split Viewer website',
 			description:
 				'A website that allows you to view multiple Twitch streams at once, splitting the screen into multiple video players.',
-			date: 'Feb 2026',
+			date: '2026-02',
 			url: 'https://twitchsplitviewer.pages.dev',
 			github: 'https://github.com/LeightonGuang/twitch-split-viewer',
 			technologies: ['Vite', 'React', 'TypeScript', 'Tailwind CSS', 'Framer Motion'],
@@ -54,7 +54,7 @@ export function getProjects(): Project[] {
 		{
 			title: 'Stream Track Chrome Extention',
 			description: '',
-			date: 'Mar 2024',
+			date: '2024-03',
 			url: 'https://chromewebstore.google.com/detail/stream-track-track-your-f/gefllgcgjeonfffgimbgfkpkpckhppdg',
 			technologies: [],
 			images: []
@@ -63,7 +63,7 @@ export function getProjects(): Project[] {
 			title: 'The Next Typer typing test website',
 			description:
 				"The Next Typer is a feature rich typing test website built with Next.js, Shadcn and Recharts. Test your speed and accuracy and track your progress with Recharts. This revamped version enahances the original typing test website using technologies I've recently learned.",
-			date: 'Mar 2025',
+			date: '2025-03',
 			github: 'https://github.com/LeightonGuang/typing_test_react_app',
 			url: 'https://thenexttyper.netlify.app',
 			technologies: ['Next.js', 'TypeScript', 'Shadcn', 'Recharts', 'Framer Motion'],
--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -2,25 +2,19 @@ import ProjectRow from './ProjectRow'
 import { getProjects } from '../../../lib/getProjects'
 
 const Projects = () => {
-	const projects = getProjects()
+	const projects = getProjects().sort((a, b) => b.date.localeCompare(a.date))
 
 	return (
-		<section className="mx-4 mb-32">
-			<table className="w-full">
-				<thead className="border-b border-zinc-500 text-left">
-					<tr className="text-muted text-sm">
-						<th className="font-light">Project</th>
-						<th className="font-light">URL</th>
-						<th className="font-light">Date</th>
-					</tr>
-				</thead>
+		<section className="mx-4 mb-32 overflow-hidden">
+			<div className="text-muted grid grid-cols-[minmax(0,1fr)_minmax(0,2fr)_auto] border-b border-zinc-500 text-sm">
+				<div>Project</div>
+				<div>URL</div>
+				<div>Date</div>
+			</div>
 
-				<tbody className="divide-y divide-zinc-500">
-					{projects.map((project) => (
-						<ProjectRow key={project.title} project={project} />
-					))}
-				</tbody>
-			</table>
+			{projects.map((project) => (
+				<ProjectRow key={project.title} project={project} />
+			))}
 		</section>
 	)
 }
