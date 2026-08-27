d0dedea594a2b877bb399ffd181618b3ce4bc348
LeightonGuang <leighton.guang@icloud.com>
2026-08-27 08:40:59 +0100

    feat: project date to show year only

--- lib/formatProjectDate.ts
+++ lib/formatProjectDate.ts
@@ -1,8 +1,16 @@
-export const formatProjectDate = (date: string) => {
+type ProjectDateFormat = 'year' | 'month-year'
+
+export const formatProjectDate = (date: string, format: ProjectDateFormat = 'year') => {
 	const [year, month] = date.split('-')
 
-	return new Date(Number(year), Number(month) - 1).toLocaleDateString('en-US', {
-		month: 'short',
-		year: 'numeric'
-	})
+	if (format === 'year') return year
+
+	if (format === 'month-year') {
+		return new Date(Number(year), Number(month) - 1).toLocaleDateString('en-US', {
+			month: 'short',
+			year: 'numeric'
+		})
+	}
+
+	throw new Error('Invalid date format')
 }
